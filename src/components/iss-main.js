86a9c16f2d7c54abdebe6d155d74b4a882c2cf9d
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-19 13:40:15 -0400

    add issviwer component

--- src/components/iss-main.js
+++ src/components/iss-main.js
@@ -1,6 +1,7 @@
 let React = require('react');
 let CurrentLocation = require('./location.js');
 let CurrentStats = require('./stats.js');
+let IssViewer = require('./stream.js');
 let connect = require('react-redux').connect;
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -12,6 +13,7 @@ const IssMain = React.createClass({
                     <h1>ISS Spotter!</h1>
                     <CurrentLocation />
                     <CurrentStats />
+                    <IssViewer />
                 </div>
             </MuiThemeProvider>
         )
