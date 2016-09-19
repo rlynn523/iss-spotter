a831ac016feaf6e7a7d7b2b7077992d01f28655d
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-19 13:48:22 -0400

    updated store for combineReducers

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ let redux = require('redux');
 let createStore = redux.createStore;
 let applyMiddleware = redux.applyMiddleware;
 let thunk = require('redux-thunk').default;
+let reducers = require ('./reducers/index');
 
-let reducers = require('./reducers/location-reducer.js');
+let store = createStore(reducers.default, applyMiddleware(thunk));
 
-let store = createStore(reducers.LocationReducer, applyMiddleware(thunk));
 module.exports = store;
