3f87b1c29b5a5e10e11c73dc948af3a8d65af13b
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-19 23:05:44 -0400

    integrated google material design paper to layout

--- src/components/location.js
+++ src/components/location.js
@@ -1,6 +1,9 @@
 let React = require('react');
 let connect = require('react-redux').connect;
 let actions = require('../actions/location.js');
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from '../../node_modules/react-tap-event-plugin';
 import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
@@ -24,30 +27,36 @@ const CurrentLocation = React.createClass({
     },
     render: function() {
         return(
-            <div className = 'location'>
-                <h3>Current Coordinates</h3>
-                <div className='latitude'>{this.props.latitude}</div>
-                <div className='longitude'>{this.props.longitude}</div>
-                <RaisedButton className='coordinateButton' label='Update ISS Position' onClick={this.updateCoordinates} />
-                <Gmaps className='map'
-                    width={'600px'}
-                    height={'400px'}
-                    lat={this.props.mapLt}
-                    lng={this.props.mapLng}
-                    zoom={3}
-                    loadingMessage={'Loading...'}
-                    params={{v: '3.exp', key: 'AIzaSyBr5i2yKpltIIb5JX2n8JweakAfWuNwZyM'}}
-                    onMapCreated={this.onMapCreated}>
-                    <Marker
-                        lat={this.props.mapLt}
-                        lng={this.props.mapLng}
-                        draggable={true}
-                        onDragEnd={this.onDragEnd} />
-                </Gmaps>
-            </div>
+            <MuiThemeProvider>
+                <div className = 'location'>
+                    <Paper zDepth={2}>
+                        <Gmaps className='map'
+                            width={'33%'}
+                            height={'400px'}
+                            lat={this.props.mapLt}
+                            lng={this.props.mapLng}
+                            zoom={3}
+                            loadingMessage={'Loading...'}
+                            params={{v: '3.exp', key: 'AIzaSyBr5i2yKpltIIb5JX2n8JweakAfWuNwZyM'}}
+                            onMapCreated={this.onMapCreated}>
+                            <Marker
+                                lat={this.props.mapLt}
+                                lng={this.props.mapLng}
+                                draggable={true}
+                                onDragEnd={this.onDragEnd} />
+                        </Gmaps>
+                        <div className='coordinateInfo'>
+                            <h3>Current Coordinates</h3>
+                            <div className='latitude'>{this.props.latitude}</div>
+                            <div className='longitude'>{this.props.longitude}</div>
+                            <RaisedButton className='coordinateButton' label='Update ISS Position' onClick={this.updateCoordinates} />
+                        </div>
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
         )
     }
-})
+});
 
 let mapStateToProps = function(state, props) {
     return {
