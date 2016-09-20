8cd440bc33d9d7624cd47e5b94cae2bd5567ec2f
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-19 23:06:29 -0400

    added basic styling, created icon button to link to map route

--- src/components/iss-main.js
+++ src/components/iss-main.js
@@ -3,17 +3,45 @@ let CurrentLocation = require('./location.js');
 let CurrentStats = require('./stats.js');
 let IssViewer = require('./stream.js');
 let connect = require('react-redux').connect;
+var router = require('react-router');
+var Link = router.Link;
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import SocialPublic from 'material-ui/svg-icons/social/public';
+
+const style = {
+    largeIcon: {
+        width: 60,
+        height: 60,
+    },
+    color: 'white',
+    hoverColor: '#9E9E9E',
+    large: {
+        width: 120,
+        height: 120,
+        padding: 30,
+  },
+};
 
 const IssMain = React.createClass({
     render: function() {
         return(
             <MuiThemeProvider>
-                <div className='iss'>
-                    <h1>ISS Spotter!</h1>
-                    <CurrentLocation />
-                    <CurrentStats />
-                    <IssViewer />
+                <div>
+                    <Paper zDepth={2}>
+                        <AppBar className='appBar' title='ISS Spotter' style={{backgroundColor: '#303F9F'}}/>
+                        <div className='mainDiv'>
+                            <p className='title' style={{fontWeight: 'lighter'}}>Where above Earth is the ISS?</p>
+                            <Link to={'/map/'}><IconButton iconStyle={style.largeIcon} style={style.large}>
+                                <SocialPublic className='launchButton' color={style.color} hoverColor={style.hoverColor} />
+                            </IconButton></Link>
+                            <p className='launchApp' style={{fontWeight: 'lighter'}}>Launch</p>
+                        </div>
+                    </Paper>
                 </div>
             </MuiThemeProvider>
         )
