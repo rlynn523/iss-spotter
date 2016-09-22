12b8fa9b3c07fb7e330cd6a44553446fa5743e2b
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-22 14:21:53 -0400

    added className to MuiThemeProvider

--- src/components/stream.js
+++ src/components/stream.js
@@ -8,7 +8,7 @@ import ReactTooltip from 'react-tooltip'
 export default class IssViewer extends Component {
     render() {
         return(
-            <MuiThemeProvider>
+            <MuiThemeProvider className='streamStyle'>
                 <Paper className='paperStream' data-tip='Double Click For Fullscreen' zDepth={2}>
                     <iframe className='videoStream' src='http://www.ustream.tv/embed/17074538?html5ui&autoplay=true&controls=false&allowfullscreen=true' frameBorder='0'></iframe>
                     <ReactTooltip place='bottom' />
