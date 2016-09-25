aa0bcc30ba1a4ff18380a45232973d70c7dd1dfe
Ryan Lynn <ryanlynn523@gmail.com>
2016-09-25 14:21:20 -0400

    added test for navbar component

--- test/navbar-test.js
+++ test/navbar-test.js
@@ -4,4 +4,17 @@ import { should } from 'chai';
 should();
 import { Provider } from 'react-redux';
 import store from '../src/store';
-import { CurrentStats } from '../src/components/stats.js'
+import NavBar from '../src/components/navbar'
+
+describe('NavBar component', function() {
+    it('Should Render NavBar Component with Links', function(){
+        let renderer = TestUtils.createRenderer();
+        renderer.render(<NavBar />);
+        let result = renderer.getRenderOutput();
+        result.props.children.props.className.should.equal('appBar');
+        result.props.children.props.title.should.equal('ISS Spotter');
+        result.props.children.props.iconElementRight.props.children[0].props.to.should.equal('/');
+        result.props.children.props.iconElementRight.props.children[1].props.to.should.equal('/map');
+        result.props.children.props.iconElementRight.props.children[2].props.to.should.equal('/stream');
+    });
+});
